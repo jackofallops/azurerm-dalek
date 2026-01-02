b50558dd52a3f1f3b2fc946bf9c987c2615b0a27
Todd Giguere <todd.giguere@ibm.com>
2026-01-02 13:48:39 -0500

    fix: allow specific subnet errors to log only and continue

--- dalek/cleaners/resource_group_network_subnet.go
+++ dalek/cleaners/resource_group_network_subnet.go
@@ -3,6 +3,7 @@ package cleaners
 import (
 	"context"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/hashicorp/go-azure-helpers/lang/pointer"
@@ -45,7 +46,7 @@ func (networkSubnetPropertiesCleaner) Cleanup(ctx context.Context, id commonids.
 				for _, sub := range *subnetList.Model {
 					// Updates/deletes are not allowed on a subnet if there are existing orphan network integragions.
 					// Call a special purge API that will clean up any integrations before we attempt to update.
-					fmt.Printf("[DEBUG]   Purging unused network integrations for Network Subnet %s", *sub.Id)
+					log.Printf("[DEBUG]   Purging unused network integrations for Network Subnet %s", *sub.Id)
 
 					webProviderLocationId := resourceproviders.ProviderLocationId{
 						SubscriptionId: id.SubscriptionId,
@@ -54,10 +55,11 @@ func (networkSubnetPropertiesCleaner) Cleanup(ctx context.Context, id commonids.
 
 					err := purgeUnusedVnetIntegrations(ctx, webProviderLocationId, *sub.Id, client.ResourceManager.WebResourceProviderClient)
 					if err != nil {
-						return fmt.Errorf("purging unused network integrations for Subnet %s: %+v", *sub.Id, err)
+						// log the error only, this may not be required for next step and should not return
+						log.Printf("[ERROR] purging unused network integrations for Subnet %s: %+v", *sub.Id, err)
 					}
 
-					fmt.Printf("[DEBUG]   Updating default properties for Network Subnet %s", *sub.Id)
+					log.Printf("[DEBUG]   Updating default properties for Network Subnet %s", *sub.Id)
 
 					subnetId, err := commonids.ParseSubnetID(*sub.Id)
 					if err != nil {
@@ -69,7 +71,9 @@ func (networkSubnetPropertiesCleaner) Cleanup(ctx context.Context, id commonids.
 					sub.Properties.PrivateEndpointNetworkPolicies = pointer.To(subnets.VirtualNetworkPrivateEndpointNetworkPoliciesDisabled)
 
 					if _, err := client.ResourceManager.NetworkSubnetClient.CreateOrUpdate(ctx, *subnetId, sub); err != nil {
-						return fmt.Errorf("updating properties for Subnet %s: %+v", subnetId, err)
+						// There are many cases where setting Delegations to None will fail (orphan SALs mostly).
+						// If this errors, log the error only and continue with other vnets
+						log.Printf("[ERROR] updating properties for Subnet %s: %+v", subnetId, err)
 					}
 				}
 			}
@@ -86,7 +90,7 @@ func (networkSubnetPropertiesCleaner) ResourceTypes() []string {
 
 // Deletion or updating of subnets can fail if there are existing Service Association Links (SAL).
 // It is possible that SALs can be orphans, where the service that was associated has been destroyed but SAL remains.
-// Based on the following article you can remove orphaned SALs with a special API:
+// Based on the following article you can remove orphaned SALs for Web Apps with a special API:
 // https://learn.microsoft.com/en-us/azure/app-service/overview-vnet-integration#method-1-purging-orphaned-service-association-links-sal
 // The purge API has not been added to official swagger, so does not exist as client in SDK.
 // This helper function will use existing Web ResourceProviders SDK to call the appropriate API.
